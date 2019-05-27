a85082eb0dd9a836511ab1751a4160841cf05c14
lixiang <imxiangli@qq.com>
2019-05-27 11:20:05 +0800

    Update AlipayCore.php

--- AlipayCore.php
+++ AlipayCore.php
@@ -29,7 +29,7 @@ class AlipayCore
             $arg .= $key . "=" . $val . "&";
         }
         //去掉最后一个&字符
-        $arg = substr($arg, 0, count($arg) - 2);
+        $arg = substr($arg, 0, strlen($arg) - 2);
 
         //如果存在转义字符，那么去掉转义
         if (get_magic_quotes_gpc()) {
